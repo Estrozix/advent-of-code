49e54322a4ce095ead2445e8be100e86e93eeaf1
Estrozix <estrozix@gmail.com>
2024-12-21 01:35:00 +0100

    Working path-finding

--- 2024/day-20/src/main.rs
+++ 2024/day-20/src/main.rs
@@ -10,11 +10,11 @@ fn main() {
     find_path(&map);
 }
 
-fn find_path(map: &Vec<Vec<char>>) {
+fn find_path(map: &Vec<Vec<char>>) -> Option<i32> {
     let mut start: Option<(usize, usize)> = None;
     let mut end: Option<(usize, usize)> = None;
 
-    let cost_map: Vec<Vec<i32>> = map
+    let mut cost_map: Vec<Vec<i32>> = map
         .iter()
         .map(|row| {
             row.iter()
@@ -48,17 +48,76 @@ fn find_path(map: &Vec<Vec<char>>) {
         })
         .collect();
 
+    if let Some(start) = start {
+        cost_map[start.1][start.0] = 0;
+    } else {
+        panic!("Didnt find start!");
+    }
+
+    if let None = end {
+        panic!("Didnt find end");
+    }
+
+    let mut end_cost: Option<i32> = None;
+
     while unvisited.len() > 0 {
         let smallest_id = get_smallest(&cost_map, &unvisited);
-        let current_pos = unvisited[smallest_id];
+        let current_pos = unvisited.remove(smallest_id);
+        let current_cost = cost_map[current_pos.1][current_pos.0];
 
-        if cost_map[current_pos.1][current_pos.0] == i32::MAX {
+        if current_cost == i32::MAX {
+            println!("Only inaccessible tiles left, breaking!");
             break;
+        }
+
+        if let Some(end) = end {
+            if end == current_pos {
+                println!("Found solution!");
+                end_cost = Some(current_cost);
+                break;
+            }
+        }
+
+        // TODO: Check all directions
+        // Right
+        let pos: (i32, i32) = (current_pos.0 as i32 + 1, current_pos.1 as i32);
+        if current_cost + 1 < check_pos(&cost_map, pos) {
+            cost_map[pos.1 as usize][pos.0 as usize] = current_cost + 1;
+        }
+
+        // Down
+        let pos: (i32, i32) = (current_pos.0 as i32, current_pos.1 as i32 + 1);
+        if current_cost + 1 < check_pos(&cost_map, pos) {
+            cost_map[pos.1 as usize][pos.0 as usize] = current_cost + 1;
+        }
+
+        // Left
+        let pos: (i32, i32) = (current_pos.0 as i32 - 1, current_pos.1 as i32);
+        if current_cost + 1 < check_pos(&cost_map, pos) {
+            cost_map[pos.1 as usize][pos.0 as usize] = current_cost + 1;
+        }
+
+        // Up
+        let pos: (i32, i32) = (current_pos.0 as i32, current_pos.1 as i32 - 1);
+        if current_cost + 1 < check_pos(&cost_map, pos) {
+            cost_map[pos.1 as usize][pos.0 as usize] = current_cost + 1;
         }
     }
 
-    println!("Start: {:?}, End: {:?}", start, end);
-    println!("{:?}", unvisited);
+    if let Some(end_cost) = end_cost {
+        println!("Found path, smallest cost is {}", end_cost);
+        return Some(end_cost);
+    } else {
+        return None;
+    }
+}
+
+fn check_pos(cost_map: &Vec<Vec<i32>>, pos: (i32, i32)) -> i32 {
+    if pos.0 < 0 || pos.1 < 0 {
+        return -1;
+    }
+
+    return cost_map[pos.1 as usize][pos.0 as usize];
 }
 
 fn get_smallest(map: &Vec<Vec<i32>>, unvisited: &Vec<(usize, usize)>) -> usize {
